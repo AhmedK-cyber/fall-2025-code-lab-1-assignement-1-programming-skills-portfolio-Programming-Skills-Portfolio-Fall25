8f21db3fdf6f15eca8d46446c9d611caae7d8093
AhmedK-cyber <soakamran34@gmail.com>
2025-11-24 09:46:25 +0400

    Add files via upload

---
+++ Exercise 04 -Primitive Quiz/exercise 4.py
@@ -0,0 +1,66 @@
+print("Welcome to a quiz in which you have to guess the capitals for these respective countries.")
+
+question_1 = input("What is the capital of France? :")
+if question_1.lower() == "paris":
+    print("Correct!")
+else:
+    print("Wrong! The capital of france is paris.")
+
+question_2 = input("What is the capital of Italy? :")
+if question_2.lower() == "rome":
+    print("Correct!")
+else:
+    print("Wrong! The capital of Italy is rome.")
+
+question_3 = input("What is the capital of Germany? :")
+if question_3.lower() == "berlin":
+    print("Correct!")
+else:
+    print("Wrong! The capital of Germany is berlin.")
+
+question_4 = input("What is the capital of Portugal? :")
+if question_4.lower() == "lisbon":
+    print("Correct!")
+else:
+    print("Wrong! The capital of Portugal is lisbon.")
+
+question_5 = input("What is the capital of Spain? :")
+if question_5.lower() == "madrid":
+    print("Correct!")
+else:
+    print("Wrong! The capital of Spain is madrid.")
+
+question_6 = input("What is the capital of Netherlands? :")
+if question_6.lower() == "amsterdam":
+    print("Correct!")
+else:
+    print("Wrong! The capital of Netherlands is amsterdam.")
+
+
+question_7 = input("What is the capital of Sweden? :")
+if question_7.lower() == "stockholm":
+    print("Correct!")
+else:
+    print("Wrong! The capital of Germany is stockholm.")
+
+question_8 = input("What is the capital of Switzerland? :")
+if question_8.lower() == "bern":
+    print("Correct!")
+else:
+    print("Wrong! The capital of Switzerland is bern.")
+
+question_9 = input("What is the capital of Norway? ;")
+if question_9.lower() == "oslo":
+    print("Correct!")
+else:
+    print("Wrong! The capital of Norway is oslo.")
+
+question_10 = input("What is the capital of Auatria? :")
+if question_10.lower() == "vienna":
+    print("Correct!")
+else:
+    print("Wrong! The capital of Austria is vienna.")
+
+print("Hope you had fun with the quiz! Please retry if you got most of them wrong.")
+
+
