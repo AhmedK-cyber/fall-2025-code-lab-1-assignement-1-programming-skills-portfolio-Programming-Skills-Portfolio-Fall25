6da74f64b965ca9d53678dc530082c8274d9d892
AhmedK-cyber <soakamran34@gmail.com>
2025-11-24 09:45:56 +0400

    Add files via upload

---
+++ Exercise 03 - Biography/exercise 3.py
@@ -0,0 +1,7 @@
+First_name = input("Enter your first name:")
+Sur_name = input("Enter your surname:")
+
+Hometown = input("Enter your hometown:")
+age = input("Enter your age:")
+biography = f"My full name is {First_name} {Sur_name}. I am from {Hometown}. I am {age} years old."
+print(biography)
